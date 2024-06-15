b021e7e9d7387bae207d1d6a618edcc58ab68a9d
Ashutosh Sendha <148787425+vegito-git@users.noreply.github.com>
2024-06-15 03:20:36 +0000

    first commit after editing

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ashutosh Sendha",
+  title: "Full Stack Developer",
+  email: "ashu50500@gmail.com",
+  gitHub: "vegito-git",
+  instagram: "",
+  linkedIn: "ashutosh-sendha-993222221/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
